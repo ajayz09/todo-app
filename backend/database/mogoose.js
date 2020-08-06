98d03b7e7603635f4274a77bdea61a6a384af0a6
Ajay <am621@uowmail.edu.au>
2020-08-06 15:10:03 +1000

    useFindAndModify` option set to false

--- backend/database/mogoose.js
+++ backend/database/mogoose.js
@@ -1,13 +1,18 @@
-
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
 //To handle asynchronous codes
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/todomanager', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-    .then( () => console.log("Database is Connected"))
-    .catch( (error) => console.log(error));
+mongoose
+  .connect("mongodb://localhost/todomanager", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("Database is Connected"))
+  .catch(error => console.log(error));
 
-// var Users = require('../routes/Users')     
-    
-module.exports = mongoose;    
+// var Users = require('../routes/Users')
+
+module.exports = mongoose;
